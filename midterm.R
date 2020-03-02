8a62997d3d5a983071b3585aee13870083273901
Tung <tungbtruong@gmail.com>
2020-03-02 02:58:35 -0500

    Changed the model name to fit their variables

--- midterm.R
+++ midterm.R
@@ -23,48 +23,54 @@ head(suiTest)
 #####################
 # Varaibles: SuiRate, Unemp, Vet, Arrest, Mental, Disable
 # 0 variable
-regression_model0 <- lm(SuiRate~SuiRate, data = suiTrain)
+SuiRate <- lm(SuiRate~SuiRate, data = suiTrain)
 # 1 variable
-regression_model1 <- lm(SuiRate~Unemp, data = suiTrain)
-regression_model2 <- lm(SuiRate~Vet, data = suiTrain)
-regression_model3 <- lm(SuiRate~Arrest, data = suiTrain)
-regression_model4 <- lm(SuiRate~Mental, data = suiTrain)
-regression_model5 <- lm(SuiRate~Disable, data = suiTrain)
+Unemp <- lm(SuiRate~Unemp, data = suiTrain)
+Vet <- lm(SuiRate~Vet, data = suiTrain)
+Arrest <- lm(SuiRate~Arrest, data = suiTrain)
+Mental <- lm(SuiRate~Mental, data = suiTrain)
+Disable <- lm(SuiRate~Disable, data = suiTrain)
 # 2 variables
-regression_model6 <- lm(SuiRate~Unemp + Vet, data = suiTrain)
-regression_model7 <- lm(SuiRate~Unemp + Arrest, data = suiTrain)
-regression_model8 <- lm(SuiRate~Unemp + Mental, data = suiTrain)
-regression_model9 <- lm(SuiRate~Unemp = Disable, data = suiTrain)
-regression_model10 <- lm(SuiRate~Vet + Arrest, data = suiTrain)
-regression_model11 <- lm(SuiRate~Vet + Mental, data = suiTrain)
-regression_model12 <- lm(SuiRate~Vet + Disable, data = suiTrain)
-regression_model13 <- lm(SuiRate~Arrest + Mental, data = suiTrain)
-regression_model14 <- lm(SuiRate~Arrest + Disable, data = suiTrain)
-regression_model15 <- lm(SuiRate~Mental + Disable, data = suiTrain)
+Unemp_Vet <- lm(SuiRate~Unemp + Vet, data = suiTrain)
+Unemp_Arrest <- lm(SuiRate~Unemp + Arrest, data = suiTrain)
+Unemp_Mental <- lm(SuiRate~Unemp + Mental, data = suiTrain)
+Unemp_Disable <- lm(SuiRate~Unemp = Disable, data = suiTrain)
+Vet_Arrest <- lm(SuiRate~Vet + Arrest, data = suiTrain)
+Vet_Mental <- lm(SuiRate~Vet + Mental, data = suiTrain)
+Vet_Disable <- lm(SuiRate~Vet + Disable, data = suiTrain)
+Arrest_Mental <- lm(SuiRate~Arrest + Mental, data = suiTrain)
+Arrest_Disable <- lm(SuiRate~Arrest + Disable, data = suiTrain)
+Mental_Disable <- lm(SuiRate~Mental + Disable, data = suiTrain)
 # 3 variables
-regression_model16 <- lm(SuiRate~Unempt + Vet + Arrest, data = suiTrain)
-regression_model17 <- lm(SuiRate~Unemp + Vet + Mental, data = suiTrain)
-regression_model18 <- lm(SuiRate~Unemp + Vet + Disable, data = suiTrain)
-regression_model19 <- lm(SuiRate~Unemp + Arrest + Mental, data = suiTrain)
-regression_model20 <- lm(SuiRate~Unemp + Arrest + Disable, data = suiTrain)
-regression_model21 <- lm(SuiRate~Unemp + Mental + Disable, data = suiTrain)
-regression_model22 <- lm(SuiRate~Vet + Arrest + Mental, data = suiTrain)
-regression_model23 <- lm(SuiRate~Vet + Arrest + Disable, data = suiTrain)
-regression_model24 <- lm(SuiRate~Vet + Mental + Disable, data = suiTrain)
-regression_model25 <- lm(SuiRate~Arrest + Mental + Disable, data = suiTrain)
+
+Unemp_Arrest_Vet <- lm(SuiRate~Unemp + Vet + Arrest, data = suiTrain)
+Unemp_Vet_Mental <- lm(SuiRate~Unemp + Vet + Mental, data = suiTrain)
+Unemp_Vet_Disable <- lm(SuiRate~Unemp + Vet + Disable, data = suiTrain)
+Unemp_Arrest_Mental <- lm(SuiRate~Unemp + Arrest + Mental, data = suiTrain)
+Unemp_Arrest_Disable <- lm(SuiRate~Unemp + Arrest + Disable, data = suiTrain)
+Unemp_Mental_Disable <- lm(SuiRate~Unemp + Mental + Disable, data = suiTrain)
+Vet_Arrest_Mental <- lm(SuiRate~Vet + Arrest + Mental, data = suiTrain)
+Vet_Arrest_Disable <- lm(SuiRate~Vet + Arrest + Disable, data = suiTrain)
+Vet_Mental_Disable <- lm(SuiRate~Vet + Mental + Disable, data = suiTrain)
+Arrest_Mental_Disable <- lm(SuiRate~Arrest + Mental + Disable, data = suiTrain)
 # 4 variables
-regression_model26 <- lm(SuiRate~Unemp + Vet + Arrest + Mental, data = suiTrain)
-regression_model27 <- lm(SuiRate~Unemp + Vet + Arrest + Disable, data = suiTrain)
-regression_model28 <- lm(SuiRate~Unemp + Vet + Mental + Disable, data = suiTrain)
-regression_model29 <- lm(SuiRate~Unemp + Arrest + Mental + Disable, data = suiTrain)
-regression_model30 <- lm(SuiRate~Vet + Arrest + Mental + Disable, data = suiTrain)
+Unemp_Arrest_Vet_Mental <- lm(SuiRate~Unemp + Vet + Arrest + Mental, data = suiTrain)
+Unemp_Vet_Disable_Arrest <- lm(SuiRate~Unemp + Vet + Arrest + Disable, data = suiTrain)
+Unemp_Vet_Disable_Mental <- lm(SuiRate~Unemp + Vet + Mental + Disable, data = suiTrain)
+Unemp_Arrest_Mental_Disable <- lm(SuiRate~Unemp + Arrest + Mental + Disable, data = suiTrain)
+Vet_Arrest_Disable_Mental <- lm(SuiRate~Vet + Arrest + Mental + Disable, data = suiTrain)
 # 5 variables 
-regression_model31 <- lm(SuiRate~Unemp + Vet + Arrest + Mental + Disable, data = suiTrain)
-
-models <- list(regression_model0, regression_model1, regression_model2, regression_model3, regression_model4, regression_model5, regression_model6, regression_model7, regression_model8, regression_model9, regression_model10, regression_model11, regression_model12, regression_model13, regression_model14, regression_model15, regression_model16, regression_model17, regression_model18, regression_model19, regression_model20, regression_model21, regression_model22, regression_model23, regression_model24, regression_model25, regression_model26, regression_model27, regression_model28, regression_model29, regression_model30, regression_model31)
-
+Unemp_Arrest_Mental_Disable_Vet <- lm(SuiRate~Unemp + Vet + Arrest + Mental + Disable, data = suiTrain)
 
 # Very big loop
+models <- list(SuiRate,Unemp,Vet,Arrest,Mental,Disable,Unemp_Vet,Unemp_Arrest,Unemp_Mental,Unemp_Disable,Vet_Arrest,Vet_Mental,\
+Vet_Disable,Arrest_Mental,Arrest_Disable,Mental_Disable,Unemp_Arrest_Vet,Unemp_Vet_Mental,Unemp_Vet_Disable,Unemp_Arrest_Mental,\
+Unemp_Arrest_Disable,Unemp_Mental_Disable ,Vet_Arrest_Mental,Vet_Arrest_Disable,Arrest_Mental_Disable,\
+Unemp_Arrest_Vet_Mental,Unemp_Vet_Disable_Arrest,Unemp_Vet_Disable_Mental,Unemp_Arrest_Mental_Disable,Vet_Arrest_Disable_Mental,\
+Unemp_Arrest_Mental_Disable_Vet)
+
+
+
 
 for (i in models) {
   # Convert model name into text
