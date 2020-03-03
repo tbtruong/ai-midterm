1c57205fdc8b14ceaf2107e1649f6661a1421ee3
m3ow <BenMeow@dhcp-wifi-8021x-155-41-14-232.bu.edu>
2020-03-03 01:25:21 -0500

    First working draft to be checked at office hours

--- midterm.R
+++ midterm.R
@@ -3,16 +3,16 @@
 ##########################################
 library(lmtest)
 library(caret)
-setwd("/Users/BenMeow/Desktop/BU\ 20\ Spring/CS440/Midterm/")
+setwd("/Users/BenMeow/Desktop/Midterm/ai-midterm")
 # Read file
-suidat <- read.csv("/")
+suidat <- read.csv("/Users/BenMeow/Desktop/Midterm/ai-midterm/SuiRate.csv")
+head(suidat)
 #Suicide Rate Data + Variables:
-#LINK#
+#https://drive.google.com/open?id=1JFalYrbit84vgHNzYlsCnm0j2AOKyz0D#
 
 # Split data randomly into training set and test set, each with 75% and 25% of the data respectively
 set.seed(3456)
-trainData <- createDataPartition(suidat$Residual, p=.75,list=FALSE,times=1)
-head(suidat)
+trainData <- createDataPartition(suidat$SuiRate, p=.75,list=FALSE,times=1)
 
 suiTrain <- suidat[trainData,]
 suiTest <- suidat[-trainData,]
@@ -23,18 +23,18 @@ head(suiTest)
 #####################
 # Varaibles: SuiRate, Unemp, Vet, Arrest, Mental, Disable
 # 0 variable
-SuiRate <- lm(SuiRate~SuiRate, data = suiTrain)
+SuiRate0 <- lm(SuiRate~SuiRate, data = suiTrain)
 # 1 variable
-Unemp <- lm(SuiRate~Unemp, data = suiTrain)
-Vet <- lm(SuiRate~Vet, data = suiTrain)
-Arrest <- lm(SuiRate~Arrest, data = suiTrain)
-Mental <- lm(SuiRate~Mental, data = suiTrain)
-Disable <- lm(SuiRate~Disable, data = suiTrain)
+Unemp1 <- lm(SuiRate~Unemp, data = suiTrain)
+Vet1 <- lm(SuiRate~Vet, data = suiTrain)
+Arrest1 <- lm(SuiRate~Arrest, data = suiTrain)
+Mental1 <- lm(SuiRate~Mental, data = suiTrain)
+Disable1 <- lm(SuiRate~Disable, data = suiTrain)
 # 2 variables
 Unemp_Vet <- lm(SuiRate~Unemp + Vet, data = suiTrain)
 Unemp_Arrest <- lm(SuiRate~Unemp + Arrest, data = suiTrain)
 Unemp_Mental <- lm(SuiRate~Unemp + Mental, data = suiTrain)
-Unemp_Disable <- lm(SuiRate~Unemp = Disable, data = suiTrain)
+Unemp_Disable <- lm(SuiRate~Unemp + Disable, data = suiTrain)
 Vet_Arrest <- lm(SuiRate~Vet + Arrest, data = suiTrain)
 Vet_Mental <- lm(SuiRate~Vet + Mental, data = suiTrain)
 Vet_Disable <- lm(SuiRate~Vet + Disable, data = suiTrain)
@@ -63,46 +63,61 @@ Vet_Arrest_Disable_Mental <- lm(SuiRate~Vet + Arrest + Mental + Disable, data = 
 Unemp_Arrest_Mental_Disable_Vet <- lm(SuiRate~Unemp + Vet + Arrest + Mental + Disable, data = suiTrain)
 
 # Very big loop
-models <- list(SuiRate,Unemp,Vet,Arrest,Mental,Disable,Unemp_Vet,Unemp_Arrest,Unemp_Mental,Unemp_Disable,Vet_Arrest,Vet_Mental,\
-Vet_Disable,Arrest_Mental,Arrest_Disable,Mental_Disable,Unemp_Arrest_Vet,Unemp_Vet_Mental,Unemp_Vet_Disable,Unemp_Arrest_Mental,\
-Unemp_Arrest_Disable,Unemp_Mental_Disable ,Vet_Arrest_Mental,Vet_Arrest_Disable,Arrest_Mental_Disable,\
-Unemp_Arrest_Vet_Mental,Unemp_Vet_Disable_Arrest,Unemp_Vet_Disable_Mental,Unemp_Arrest_Mental_Disable,Vet_Arrest_Disable_Mental,\
+models <- list(SuiRate0,Unemp1,Vet1,Arrest1,Mental1,Disable1,Unemp_Vet,Unemp_Arrest,Unemp_Mental,Unemp_Disable,Vet_Arrest,Vet_Mental,
+Vet_Disable,Arrest_Mental,Arrest_Disable,Mental_Disable,Unemp_Arrest_Vet,Unemp_Vet_Mental,Unemp_Vet_Disable,Unemp_Arrest_Mental,
+Unemp_Arrest_Disable,Unemp_Mental_Disable ,Vet_Arrest_Mental,Vet_Arrest_Disable,Arrest_Mental_Disable,
+Unemp_Arrest_Vet_Mental,Unemp_Vet_Disable_Arrest,Unemp_Vet_Disable_Mental,Unemp_Arrest_Mental_Disable,Vet_Arrest_Disable_Mental,
 Unemp_Arrest_Mental_Disable_Vet)
 
-git 
+modelname <- list("SuiRate0","Unemp1","Vet1","Arrest1","Mental1","Disable1","Unemp_Vet","Unemp_Arrest","Unemp_Mental","Unemp_Disable","Vet_Arrest","Vet_Mental",
+"Vet_Disable","Arrest_Mental","Arrest_Disable","Mental_Disable","Unemp_Arrest_Vet","Unemp_Vet_Mental","Unemp_Vet_Disable","Unemp_Arrest_Mental",
+"Unemp_Arrest_Disable","Unemp_Mental_Disable" ,"Vet_Arrest_Mental","Vet_Arrest_Disable","Arrest_Mental_Disable",
+"Unemp_Arrest_Vet_Mental","Unemp_Vet_Disable_Arrest","Unemp_Vet_Disable_Mental","Unemp_Arrest_Mental_Disable","Vet_Arrest_Disable_Mental",
+"Unemp_Arrest_Mental_Disable_Vet")
 
+rank <- list()
 
-for (i in models) {
-  # Convert model name into text
-  name <- deparse(substitute(i))
+for (i in 1:31) {
   # R outputs to .txt file instead of consolde
-  sink(name + ".txt")
-  print(summary(i))
+  #sink(paste(modelname[i],".txt",sep="")) 
+  #print(lapply(models[i],summary))
+  summ <- lapply(models[i],summary)
+  rsquared <- summ[[1]][9]
+  #rank <- c(rank,list(rsquared,modelname[i]))
+  rank[[i]] <- c(rsquared,modelname[i])
   # Revert R outputs to console
-  sink()
-  # name to list
-  # if (____ in list)
-  # plot
-  
+  #sink()
 }
+hey <- lapply(models[2],summary)
+# Arrange list of lists by first element of sublist
+rank <- rank[order(sapply(rank,'[[',1))]
+# Print sublist with highest adjusted R squared value
+print(rank[[31]])
+# We observe that, in the 5 variables model, the variable Disable was not statistically significant
+# We then take the 2nd best model
+print(rank[[30]])
+best_model <- Unemp_Arrest_Vet_Mental
 
 #Error term analysis
 ####################
-error_terms <- multiple_regression_model$residuals # from the fit
-yhTrain$Residual  - predict(multiple_regression_model, yhTrain) 
+error_terms <- best_model$residuals # from the fit
+#All the error terms
+suiTrain$SuiRate - predict(best_model, suiTrain) 
 
 ###Linear Response
-plot(suiTrain$Unemp,yhTrain$Residual)
-plot(suiTrain$Vet,yhTrain$Residual)
-plot(suiTrain$Arrest,yhTrain$Residual)
-plot(suiTrain$Mental,yhTrain$Residual)
-plot(suiTrain$Disable,yhTrain$Residual)
-# Exponential relationship ?!??!?!?!?
-plot(yhTrain$Froude,yhTrain$Residual)
+plot(suiTrain$Unemp,suiTrain$SuiRate)
+lines(suiTrain$Unemp, fitted(Unemp1), col="blue")
+plot(suiTrain$Vet,suiTrain$SuiRate)
+lines(suiTrain$Vet, fitted(Vet1), col="red")
+plot(suiTrain$Arrest,suiTrain$SuiRate)
+lines(suiTrain$Arrest, fitted(Arrest1), col="green")
+plot(suiTrain$Mental,suiTrain$SuiRate)
+lines(suiTrain$Mental, fitted(Mental1), col="yellow")
+#plot(suiTrain$Disable,suiTrain$SuiRate)
 
 ### Errors independent and and variance about the same
 plot(error_terms)
-lmtest::bptest(multiple_regression_model)
+lmtest::bptest(best_model)
 
 ###Approximately Normal?
 hist(error_terms)
@@ -112,15 +127,16 @@ qqline(error_terms)
 ###############
 # Prediction in R
 ###########
-predict(multiple_regression_model, yhTest)
-yhTest$Residual - predict(multiple_regression_model, yhTest)
+predict(best_model, suiTest)
+suiTest$SuiRate - predict(best_model, suiTest)
 
 # Calculate mean and standard deviation for test and training residuals
-mean(yhTest$Residual - predict(multiple_regression_model, yhTest))
-sd(yhTest$Residual - predict(multiple_regression_model, yhTest))
+mean(suiTest$SuiRate - predict(best_model, suiTest))
+sd(suiTest$SuiRate - predict(best_model, suiTest))
 
-mean(yhTrain$Residual - predict(multiple_regression_model, yhTrain))
-sd(yhTrain$Residual - predict(multiple_regression_model, yhTrain))
+mean(suiTrain$SuiRate - predict(best_model, suiTrain))
+sd(suiTrain$SuiRate - predict(best_model, suiTrain))
+
 
 # Determine observation range between training and test set
 min(yhTrain$Froude)
